1dd0ff52f169d63cf3c4e9d2d8919adeb6b69cb2
theo obriyan <theo obriyan>
2016-09-18 14:32:03 +0530

    unwanted block of code removed

--- StuInfo.java
+++ StuInfo.java
@@ -12,11 +12,6 @@ class Student
 	}
     Student(int sno,String sname,float marks)
 	{
-		/*System.out.println("Static Block Called");
-		System.out.println("Static Block Called");
-		System.out.println("Static Block Called");
-		System.out.println("Static Block Called");
-		System.out.println("Static Block Called");*/
 		System.out.println("sno="+sno+"   "+"sname="+sname+"  "+"marks="+marks+"  "+"crse="+crse);
 	}
 
@@ -27,7 +22,7 @@ class StuInfo
 {
 	public static void main(String[] args) 
 	{
-		//System.out.println("Hello World0
+		
 		Student s1=new Student(1,"Priyanka",89.65f);
 		Student s2=new Student(2,"Nashra",99.65f);
 		Student s3=new Student(3,"Mayank",89.65f);
